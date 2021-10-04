8555201e08fabce0787df0518f0a31599078b867
d47sec <datdo.230901@gmail.com>
2021-10-05 06:43:58 +0700

    add files

---
+++ Hackerrank/Python/Math/power_modpower.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+print(pow(a, b))
+# a mũ b sau đó lấy dư cho m  
+print(pow(a, b,m))

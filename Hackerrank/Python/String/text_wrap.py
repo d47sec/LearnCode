e2bfa69427bcdfe65f5d68f84fdeac7cb4e3c06e
d47sec <datdo.230901@gmail.com>
2021-10-05 06:33:53 +0700

    add text_wrap.py

---
+++ Hackerrank/Python/String/text_wrap.py
@@ -0,0 +1,13 @@
+
+def wrap(string, max_width):
+    len_string = len(string)
+    result = ''
+    for i in range(0,len_string,max_width):
+        result += string[i:i+max_width] + "\n"
+    
+    return result 
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)

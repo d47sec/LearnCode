a9a07da3179631068063de1e457081d6cd75a9c0
d47sec <datdo.230901@gmail.com>
2021-10-06 10:18:02 +0700

    update file

--- Hackerrank/Python/itertools/product.py
+++ Hackerrank/Python/itertools/product.py
@@ -3,6 +3,11 @@ from itertools import product
 A = list(int(i) for i in input().split(" "))
 B = list(int(i) for i in (input().split(" ")))
 # (print(i) for i in list(product(A,B)))
+# hàm product này giống như tính tích đề cát của 2 vector 
+# VÍ DỤ:
+# A = [1, 2]
+# B = [3, 4]
+# AxB = [(1, 3), (1, 4), (2, 3), (2, 4)]
 C = list(product(A,B))
 for i in C:
     print(i, end=" ")

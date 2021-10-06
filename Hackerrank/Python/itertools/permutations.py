890c2860b1b053dabf41053d974aa45916737f3b
d47sec <datdo.230901@gmail.com>
2021-10-06 10:28:19 +0700

    add

---
+++ Hackerrank/Python/itertools/permutations.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+# permutations là hàm dùng để hoán vị các phần tử trong một iterator 
+value = list(input().split(" "))
+S = value[0]
+k = int(value[1])
+# print(list(permutations(S, k)))
+array = []
+for i in list(permutations(S,k)):
+    array.append(''.join(i))
+array.sort()
+for i in array:
+    print(i)

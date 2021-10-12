65b32fae16d02ca3762a7b02058ae1c0207c864a
d47sec <datdo.230901@gmail.com>
2021-10-12 15:09:02 +0700

    phai dau pv vcs

---
+++ Hackerrank/Algorithms/Recusrion/the_power_sum.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'powerSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER X
+#  2. INTEGER N
+#
+
+def powerSum(X, N):
+    # Write your code here
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    X = int(input().strip())
+
+    N = int(input().strip())
+
+    result = powerSum(X, N)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

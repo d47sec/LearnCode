241c1e031f9f1d6ef333559a6b8f1eaf3591ff6b
d47sec <datdo.230901@gmail.com>
2021-10-13 16:54:13 +0700

    update file

--- LeetCode/Algo/Day2 Two Pointers/squares_of_sorted_array.py
+++ LeetCode/Algo/Day2 Two Pointers/squares_of_sorted_array.py
@@ -1,10 +1,22 @@
 class Solution:
     def sortedSquares(self, nums):
-        return sorted([i*i for i in nums])
+        left, right, i = 0, len(nums) - 1, len(nums) - 1
+        output = [0] * len(nums)
+        while left <= right:
+            a = nums[left] * nums[left]
+            b = nums[right] * nums[right]
+            if a > b:
+                output[i] = a
+                left += 1
+            else:
+                output[i] = b
+                right -= 1
+            i -= 1
+        return output
 
-# bài nào đơn giản được thì đơn giản two pointer bài này rắc rồi :)))
-
+# Độ phức tạp thuật toán là 0(log(n))
+# Độ phức tạp bộ nhớ là 0(N)
 solve = Solution()
-nums = [-1, -2, 0, 1, 2, 4]
+nums = [-4,-1,0,3,5]
 print(solve.sortedSquares(nums))
 

68c30b6f7b1c79427d2a2d662cf387a0eb92449a
d47sec <datdo.230901@gmail.com>
2021-10-14 06:41:05 +0700

    add files

---
+++ LeetCode/Algo/Day2 Two Pointers/contain_most_water.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        k = len(height)
+        area = 0
+        l = 0
+        r = k - 1
+        max_width = k - 1
+        for width in range(max_width, 0, -1):
+            if(height[l] < height[r]):
+                area = max(area, width * height[l])
+                l += 1
+            else:
+                area = max(area, width * height[r])
+                r -= 1
+        return area 
+            
+        
+# Link: https://leetcode.com/problems/container-with-most-water/
+# TWO POINTERS 
+            
+            
+        

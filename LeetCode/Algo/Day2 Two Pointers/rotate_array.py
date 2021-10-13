304cc6ef3ffce9e2ef236fbb6109eb9a4eb77084
d47sec <datdo.230901@gmail.com>
2021-10-13 17:14:54 +0700

    Day 2 two pointers

--- LeetCode/Algo/Day2 Two Pointers/rotate_array.py
+++ LeetCode/Algo/Day2 Two Pointers/rotate_array.py
@@ -3,7 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # đang sử dụng high function vì sài hàm trong hàm 
         def twopt(arr, i, j):
+            # function này có chức năng chia mảng thành 2 bên sau đó hoán đổi giá trị của 2 mảng con này cho nhau 
             while i < j:
                 arr[i] , arr[j] = arr[j], arr[i]
                 i += 1
@@ -18,7 +20,7 @@ class Solution:
             
                 
 nums =[-4, -3, -2, 0, 1, 2]
-k = 3
+k = 2
 solve = Solution()
 solve.rotate(nums, k)
 print(nums)

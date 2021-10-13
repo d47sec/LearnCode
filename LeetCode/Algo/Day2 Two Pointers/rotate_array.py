6b1f024a9a3635cf815c8111ab5f36e51d73969e
d47sec <datdo.230901@gmail.com>
2021-10-13 17:09:09 +0700

    Day 2 two pointers

--- LeetCode/Algo/Day2 Two Pointers/rotate_array.py
+++ LeetCode/Algo/Day2 Two Pointers/rotate_array.py
@@ -3,11 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        while k:
-            nums.insert(0, nums.pop())
-            k -= 1
-
-nums =[1, 2, 3, 4, 5, 6, 7]
+        def twopt(arr, i, j):
+            while i < j:
+                arr[i] , arr[j] = arr[j], arr[i]
+                i += 1
+                j -= 1
+            return arr
+        if k > 0:
+            twopt(nums, 0, len(nums)-1)
+            print(nums)
+            twopt(nums, 0, k -1)
+            print(nums)
+            twopt(nums, k, len(nums)-1)
+            
+                
+nums =[-4, -3, -2, 0, 1, 2]
 k = 3
 solve = Solution()
 solve.rotate(nums, k)

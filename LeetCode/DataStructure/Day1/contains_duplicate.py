45550c0bc6756c0505bdadaad66b60faab794c55
d47sec <datdo.230901@gmail.com>
2021-10-14 06:59:12 +0700

    add file

--- LeetCode/DataStructure/Day1/contains_duplicate.py
+++ LeetCode/DataStructure/Day1/contains_duplicate.py
@@ -1,8 +1,18 @@
 class Solution:
-    def containsDuplicate(self, nums) -> bool:
-        sets = set(nums)
-        return True if len(nums) != len(sets) else False
-
+    # Cách 1 : Sài set trong python 
+    # def containsDuplicate(self, nums) -> bool:
+    #     return True if len(nums) != len(set(nums)) else False
+    
+    # Cách 2: Sài dictionary
+    # tốc độ nhanh hơn 
+    def containsDuplicate(self, nums)->bool:
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                return True
+            dic[i] = 1
+        return False
+    
 nums = [1,1,1,3,3,4,3,2,4,2]
 solve = Solution()
 print(solve.containsDuplicate(nums))

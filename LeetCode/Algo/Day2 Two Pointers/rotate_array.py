993b57957a94cd0f94859b90d481b0f74978f8b6
d47sec <datdo.230901@gmail.com>
2021-10-13 21:01:32 +0700

    update

--- LeetCode/Algo/Day2 Two Pointers/rotate_array.py
+++ LeetCode/Algo/Day2 Two Pointers/rotate_array.py
@@ -12,13 +12,12 @@ class Solution:
                 j -= 1
             return arr
         if k > 0:
-            twopt(nums, 0, len(nums)-1)
-            print(nums)
-            twopt(nums, 0, k -1)
-            print(nums)
-            twopt(nums, k, len(nums)-1)
+            twopt(nums, 0, len(nums)-1) # xoay toàn bộ mảng
+            # print(nums)
+            twopt(nums, 0, k -1) # xoay số phần tử cần phải xoay theo yêu cầu đề
+            # print(nums)
+            twopt(nums, k, len(nums)-1) # xoay số phần tử còn lại trong mảng
             
-                
 nums =[-4, -3, -2, 0, 1, 2]
 k = 2
 solve = Solution()

6121ebabfa1506e981989b9513902596a1873b12
d47sec <datdo.230901@gmail.com>
2021-10-14 23:40:21 +0700

    Algo Day3

---
+++ LeetCode/Algo/Day3 Two Pointers/move_zeros.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
+
+            
+solve = Solution()
+nums = [0,1,0,3,12]
+solve.moveZeroes(nums)
+print(nums)
+# Output: [1,3,12,0,0]

b9da1bbb731502566b7601dee9bbe65b70d36bfe
d47sec <datdo.230901@gmail.com>
2021-10-17 22:48:23 +0700

    Algo DAY5

---
+++ Hackerrank/Algorithms/Strings/super_reduced_string.py
@@ -0,0 +1,19 @@
+def superReducedString(s):
+    s = list(s)
+    i = 0
+    while i < len(s) - 1:
+        if s[i] == s[i+1]:
+            del s[i]
+            del s[i]
+            i = 0 
+            if len(s) == 0:
+                return 'Empty String'
+        else:
+            i += 1
+    return ''.join(s)
+
+print(superReducedString('aaabccddd'))
+            
+    
+        
+    

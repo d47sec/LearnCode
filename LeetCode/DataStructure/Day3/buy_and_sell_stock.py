1cf3aded74671f3c495a1883773dc96b8361836b
d47sec <datdo.230901@gmail.com>
2021-10-17 07:44:35 +0700

    DS DAY3

--- LeetCode/DataStructure/Day3/buy_and_sell_stock.py
+++ LeetCode/DataStructure/Day3/buy_and_sell_stock.py
@@ -7,7 +7,7 @@ class Solution:
         #         profit = prices[j] - prices[i]
         #         if profit > max_profit:
         #             max_profit = profit 
-        # return max_profit
+        # return max_profits
     
         # SOLUTION 2:
         max_profit = 0 
@@ -24,3 +24,23 @@ class Solution:
 solve = Solution()
 prices = [7,6,4,3,13]
 print(solve.maxProfit(prices))
+
+# JAVA:
+# public class Solution {
+#     public static int maxProfit(int[] prices) {
+#         int max_profit = 0;
+#         int buy = prices[0];
+#         int profit;
+#         for(int i = 1; i < prices.length; i ++){
+#             profit = prices[i] - buy;
+#             if(profit > max_profit){
+#                 max_profit = profit;
+#             }
+#             if(buy > prices[i]){
+#                 buy = prices[i];
+#             }
+#         }
+#         return max_profit;
+#     }
+# }
+

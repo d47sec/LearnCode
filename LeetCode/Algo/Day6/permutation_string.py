907dd2453c3da7e4e7a8e44f60d1a9bd05acb42b
d47sec <datdo.230901@gmail.com>
2021-10-18 23:24:39 +0700

    Algo DAY6

---
+++ LeetCode/Algo/Day6/permutation_string.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # Sử dụng kĩ thuật mảng đánh dấu 
+        array1 = [0] * 26
+        for i in s1:
+            array1[ord(i) - 97] += 1
+        
+        for i in range(len(s2)-len(s1)+1):
+            array2 = [0] * 26 
+            for j in range(len(s1)):
+                array2[ord(s2[i+j])-97] += 1
+            if self.match(array1, array2):
+                return True
+        return False
+    
+    def match(self, array1, array2):
+        for i in range(len(array1)):
+            if array1[i] != array2[i]:
+                return False
+        return True
+        
+           
+solve = Solution()
+print(solve.checkInclusion('ab', 'eidbaooo'))
+    

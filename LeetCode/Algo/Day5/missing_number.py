cbdbf7e2ee2f66d93205eed4fd9314af2f7f3e43
d47sec <datdo.230901@gmail.com>
2021-10-18 14:07:13 +0700

    Algo DAY5

---
+++ LeetCode/Algo/Day5/missing_number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def missingNumber(self, nums) -> int:
+        n = len(nums)
+        # sum of nums from 1 to n 
+        sum_n = n * (n+1) // 2
+        for num in nums:
+            sum_n -= num
+        return sum_n
+                
+solve = Solution()
+nums = [9,6,4,2,3,5,7,0,1]
+print(solve.missingNumber(nums))
+# https://leetcode.com/problems/missing-number/

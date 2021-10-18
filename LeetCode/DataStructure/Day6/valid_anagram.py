22e0a7ca3ceadb077d60ea768db1933039bc9868
d47sec <datdo.230901@gmail.com>
2021-10-18 08:58:16 +0700

    DS DAY6

---
+++ LeetCode/DataStructure/Day6/valid_anagram.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dic1 = {}
+        dic2 = {}
+        for i in s:
+            if i in dic1:
+                dic1[i] += 1
+            else:
+                dic1[i] = 1
+        
+        for i in t:
+            if i in dic2:
+                dic2[i] += 1
+            else:
+                dic2[i] = 1
+        # print(dic1)
+        # print(dic2)
+        if len(dic1) != len(dic2):
+            return False
+        for i in dic1:
+            if i in dic2 and dic1[i] == dic2[i]:
+                continue
+            else:
+                return False
+        return True    
+
+solve = Solution()
+print(solve.isAnagram("rat", "car"))

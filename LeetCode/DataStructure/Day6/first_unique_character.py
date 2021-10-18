b7abd20a9bf9dfb7e12f5db75958e84f44cb1ab5
d47sec <datdo.230901@gmail.com>
2021-10-18 08:20:42 +0700

    DS DAY6

---
+++ LeetCode/DataStructure/Day6/first_unique_character.py
@@ -0,0 +1,26 @@
+import collections
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        # SOLUTION 1:
+        dic = {}
+        for i, c in enumerate(s):
+            if c in dic:
+                dic[c] += 1
+            else:
+                dic[c] = 1 
+        for i, c in enumerate(s):
+            if dic[c] == 1:
+                return i
+        return -1
+        # SOLUTION 2
+        # count = collections.Counter(s)
+        # print(count)
+        # # find the index
+        # for idx, ch in enumerate(s):
+        #     if count[ch] == 1:
+        #         return idx     
+        # return -1
+        
+solve = Solution()
+print(solve.firstUniqChar('loveleetcode'))
+                

96479908dbe25dfee4b857dda228420728a3933c
d47sec <datdo.230901@gmail.com>
2021-10-21 07:24:02 +0700

    Algo Day8

---
+++ LeetCode/Algo/Day8/largest_number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestNumber(self, nums) -> str:
+        result = ""
+        
+        
+        
+        
+    
+solve = Solution()    
+nums = [3,30,34,5,9]
+print(solve.largestNumber(nums))
+# Output: "9534330"
+        

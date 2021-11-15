5771c2f82c7890dc1e26fe9ad4270096628f9444
d47sec <datdo.230901@gmail.com>
2021-11-15 09:29:56 +0700

    DS

--- LeetCode/DataStructure/Array/matrix_spiral.py
+++ LeetCode/DataStructure/Array/matrix_spiral.py
@@ -1,8 +1,9 @@
 def spiral(arr):
     top, left = 0 , 0 
-    bottom, right = len(arr) - 1, len(arr) - 1 
+    bottom, right = len(arr) - 1, len(arr[0]) - 1 
     res = []
     while top <= bottom and left <= right:
+        
         for i in range(left, right+1):
             res.append(arr[top][i])
         top += 1
@@ -11,22 +12,29 @@ def spiral(arr):
             res.append(arr[i][right])
         right -= 1
 
-        for i in range(right, left - 1, -1):
-            res.append(arr[bottom][i])
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                res.append(arr[bottom][i])
+            
+            bottom -= 1
         
-        bottom -= 1
-
-        for i in range(bottom, top -1, -1):
-            res.append(arr[i][left])
-        
-        left += 1
-
+        if left <= right:
+            for i in range(bottom, top -1, -1):
+                res.append(arr[i][left])
+            left += 1
+            
     return res 
 
 
 a = [[1, 2, 3, 18],
     [5, 6, 7, 17],
-    [9, 10, 11,16],
-    [12,13,14,15]]
+    [9, 10, 11,16]]
 
 print(spiral(a))
+
+# +) Ý TƯỜNG LÀ GÌ ? 
+# - cần tạo bốn giá trị tương ứng với 4 góc: top, left, bottom, right
+# - sau đó di chuyển từ trái sang phái : top += 1 
+# - sau đó di chuyển từ trên xuống dưới: right -= 1
+# - sau đó di chuyển từ phải sang trái : bottom -= 1
+# - sau đó di chuyển từ dưới lên trên : left += 1 

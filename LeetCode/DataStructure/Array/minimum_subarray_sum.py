2d95be6072b7d871a16b08b91b457e7f688ce6fc
d47sec <datdo.230901@gmail.com>
2021-11-17 09:09:02 +0700

    DS

--- LeetCode/DataStructure/Array/minimum_subarray_sum.py
+++ LeetCode/DataStructure/Array/minimum_subarray_sum.py
@@ -1,11 +1,18 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums) -> int:
-        pass
-    
-
-
-
-
+        left = 0 
+        sum = 0 
+        ans = len(nums) + 1
+        for right in range(len(nums)):
+            sum += nums[right]
+            while sum >= target and left <= right:
+                ans = min(ans, right+1-left)
+                sum -= nums[left]
+                left += 1
+        
+        return ans if ans != len(nums)+1 else 0
+       
+        
 solve = Solution()
 target = 7
 nums = [2,3,1,2,4,3]

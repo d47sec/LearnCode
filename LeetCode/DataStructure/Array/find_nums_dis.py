08ba566cb3e5b8f343aaf9063e18b1ae919ec69d
d47sec <datdo.230901@gmail.com>
2021-11-24 13:25:24 +0700

    Array

--- LeetCode/DataStructure/Array/find_nums_dis.py
+++ LeetCode/DataStructure/Array/find_nums_dis.py
@@ -1,6 +1,22 @@
-# class Solution:
-#     def findDisappearedNumbers(self, nums):
-#         dict = {}
-#         for i in nums:
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        dict = {}
+        for num in nums:
+            if num in dict:
+                dict[num] += 1
+            else:
+                dict[num] = 1
+        result = []
+        for i in range(1, len(nums)+1):
+            if i not in dict:
+                result.append(i)
+    
+        return result
+    
+
+nums = [4,3,2,7,8,2,3,1]
+solve = Solution()
+print(solve.findDisappearedNumbers(nums))
+            
             
         

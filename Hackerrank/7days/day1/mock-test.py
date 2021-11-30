e5612517db56b00f5a00524af629548464c915a1
d47sec <datdo.230901@gmail.com>
2021-11-30 23:10:34 +0700

    day1

---
+++ Hackerrank/7days/day1/mock-test.py
@@ -0,0 +1,9 @@
+def findMedian(arr):
+    arr = sorted(arr)
+    print(arr)
+    return arr[len(arr) // 2] 
+
+print(findMedian([34,2,3,4,5]))
+
+# code như này hay vc , vừa quen với áp lực thời gian , vừa bắt đầu óc nãy số nhanh 
+
